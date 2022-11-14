24b02d35098961dba18f56ba52244ed931d5707a
Vinícius_Tadeu <94875259+vinicius-tadeu@users.noreply.github.com>
2022-11-13 22:20:59 -0300

    Delete src directory

--- src/components/SectionTitle.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./styles/SectionTitle.sass";
-
-function SectionTitle({ content }) {
-  return <h1 className="title">{content}</h1>;
-}
-
-export default SectionTitle;
